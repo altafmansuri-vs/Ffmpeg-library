aca1541b62996da115e0c612ba8e7891371e68e8
hp <altafmansuri.devloper@gmail.com>
2025-04-07 15:41:52 +0530

    Added ffmpeg .so

---
+++ ffmpeg/src/main/java/com/altaf/mansuri/AsyncGetMediaInformationTask.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2018-2020 Taner Sener
+ *
+ * This file is part of MobileFFmpeg.
+ *
+ * MobileFFmpeg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MobileFFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with MobileFFmpeg.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.altaf.mansuri;
+
+import android.os.AsyncTask;
+
+/**
+ * <p>Utility class to get media information asynchronously.
+ *
+ * @author Taner Sener
+ */
+public class AsyncGetMediaInformationTask extends AsyncTask<String, com.altaf.mansuri.MediaInformation, com.altaf.mansuri.MediaInformation> {
+    private final String path;
+    private final GetMediaInformationCallback getMediaInformationCallback;
+
+    public AsyncGetMediaInformationTask(final String path, final GetMediaInformationCallback getMediaInformationCallback) {
+        this.path = path;
+        this.getMediaInformationCallback = getMediaInformationCallback;
+    }
+
+    @Override
+    protected com.altaf.mansuri.MediaInformation doInBackground(final String... arguments) {
+        return FFprobe.getMediaInformation(path);
+    }
+
+    @Override
+    protected void onPostExecute(final MediaInformation mediaInformation) {
+        if (getMediaInformationCallback != null) {
+            getMediaInformationCallback.apply(mediaInformation);
+        }
+    }
+
+}
